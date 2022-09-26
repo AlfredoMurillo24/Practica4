5db85c050d96aa7807014ac9b003ff5f3dcce59f
ANTRAXMUSA <ANTRAXMUSA@LAPTOP-O82C1MS8>
2022-09-26 10:03:58 -0700

    Add project files.

---
+++ Practica4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sl1;
+            int c;
+            double resultado;
+            double multi = 1.8;
+            int f = 32;
+
+
+            Console.WriteLine("Converitire tus centigrados a farenheit. Dime tus centigrados; ");
+            sl1 = Console.ReadLine();
+            c = Convert.ToInt32(sl1);
+            resultado = c * multi + f;
+
+            Console.WriteLine("tus farenheit son: {0}", resultado);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practica4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Practica4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Practica4")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a5602d80-af7d-47ed-819b-e81486a93e1e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
